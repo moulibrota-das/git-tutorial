3dfbfd7256a5a04565fb3acc22613a33b3afaa21
gkdey17cse <gkdey17cse@gmail.com>
2023-04-10 21:32:19 +0530

    Changes no 2

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("New Hello world Soham")
+    return HttpResponse("Hello World Gour Krishna Dey")
 
 def about(request):
     return HttpResponse("About page Gour Krishna")

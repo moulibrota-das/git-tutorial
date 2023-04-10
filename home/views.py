092b220eed48b8546e3afc696eab5002e3ce0198
gkdey17cse <gkdey17cse@gmail.com>
2023-04-10 21:13:27 +0530

    Change no 1

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ def index(request):
     return HttpResponse("New Hello world")
 
 def about(request):
-    return HttpResponse("About page")
+    return HttpResponse("About page Gour Krishna")
 
 def contacts(request):
     return HttpResponse("Contacts page")

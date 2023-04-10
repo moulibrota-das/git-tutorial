9e8fec9d6999886a6545916173d7b6763a26cfde
moulibrota-das <dasmoulibrota@gmail.com>
2023-04-10 20:42:54 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello world in django")
+
+def about(request):
+    return HttpResponse("About page")
+
+def contacts(request):
+    return HttpResponse("Contacts page")
+
+def services(request):
+    return HttpResponse("Services page")
